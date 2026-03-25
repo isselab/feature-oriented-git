7dfd1fc89f5868caf127ffa7685c9547534eaa0d
Aashutosh A. <git.circulate317@passinbox.com>
2026-03-25 11:48:15 +0100

    feat: add variant management support init command

--- src/commands/init.rs
+++ src/commands/init.rs
@@ -1,6 +1,29 @@
-use anyhow::Result;
+use std::fs;
+use std::io::Write;
+
+use anyhow::{Context, Result};
 use git2::Repository;
 
-pub fn run(_repo: &Repository) -> Result<()> {
-    todo!("Initialize fog in the repository");
+const DEFAULT_CONFIG: &str = r#"# git-varcs configuration file
+# You can define variants and associate features here
+"#;
+
+pub fn run(repo: &Repository) -> Result<()> {
+    let base_path = repo
+        .path()
+        .parent()
+        .context("Repository has no parent directory")?;
+    let config_path = base_path.join("varcs.toml");
+
+    let mut file = fs::OpenOptions::new()
+        .write(true)
+        .create_new(true)
+        .open(&config_path)
+        .with_context(|| format!("Failed to create config file at path: {:?}", config_path))?;
+
+    file.write(DEFAULT_CONFIG.as_bytes())
+        .with_context(|| format!("Failed to write default config to: {:?}", config_path))?;
+
+    println!("Initialized config file at {}", config_path.display());
+    Ok(())
 }
