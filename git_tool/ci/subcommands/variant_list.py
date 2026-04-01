5e6119ac3b345419817ff5fa37d5de0c1e816948
Aashutosh A. <git.circulate317@passinbox.com>
2026-04-01 14:27:32 +0200

    feat: add variant list command

--- git_tool/ci/subcommands/variant_list.py
+++ git_tool/ci/subcommands/variant_list.py
@@ -1,3 +1,11 @@
+from git_tool.feature_data.models_and_context.repo_context import repo_context
+
+
 def list_variant():
     """List all the variants present in the repo"""
-    print(f"Listing all the variants in the repo!")
+
+    with repo_context() as repo:
+        for ref in repo.refs:
+            if ref.path.startswith("refs/heads/variant/"):
+                variant = ref.path.replace("refs/heads/variant/", "")
+                print(variant)
